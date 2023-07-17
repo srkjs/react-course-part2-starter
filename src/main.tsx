a754753b8fd517975d7d78ac0a59b51e25ecc8eb
swathir01 <53891830+srkjs@users.noreply.github.com>
2023-07-17 10:31:03 +0530

    Using React Query DevTools
    npm install @tanstack/react-query-devtools@4.28

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // QueryClient - core object we use for managing and caching remote data in react query
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App';
 import './index.css';
 
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools /> {/* Only for Development */}
     </QueryClientProvider>
   </React.StrictMode>
 );
