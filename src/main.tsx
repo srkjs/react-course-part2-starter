de4a26837ad71b2ee66d348568240863124f2998
swathir01 <53891830+srkjs@users.noreply.github.com>
2023-07-17 08:17:18 +0530

    Setting Up React Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// QueryClient - core object we use for managing and caching remote data in react query
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 import './index.css';
 
-ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
