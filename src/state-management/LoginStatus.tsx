ed08fc63602165e4048f196d1def6f1b113f26ce
swathir01 <53891830+srkjs@users.noreply.github.com>
2023-07-19 16:01:04 +0530

    Exercise- Working with Reducers

--- src/state-management/LoginStatus.tsx
+++ src/state-management/LoginStatus.tsx
@@ -1,14 +1,13 @@
-import { useState } from "react";
+import { useReducer, useState } from 'react';
 
 const LoginStatus = () => {
-  const [user, setUser] = useState('');
-
+  const [user, dispatch] = useReducer(authReducer, '');
   if (user)
     return (
       <>
         <div>
-          <span className="mx-2">{user}</span>
-          <a onClick={() => setUser('')} href="#">
+          <span className='mx-2'>{user}</span>
+          <a onClick={() => dispatch({ type: 'LOGOUT' })} href='#'>
             Logout
           </a>
         </div>
@@ -16,7 +15,10 @@ const LoginStatus = () => {
     );
   return (
     <div>
-      <a onClick={() => setUser('mosh.hamedani')} href="#">
+      <a
+        onClick={() => dispatch({ type: 'LOGIN', username: 'mosh.hamedani' })}
+        href='#'
+      >
         Login
       </a>
     </div>
