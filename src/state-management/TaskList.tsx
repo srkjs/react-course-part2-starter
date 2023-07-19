2905da741c1d7ce978baf4206a9e2eb39ca1a969
swathir01 <53891830+srkjs@users.noreply.github.com>
2023-07-19 11:12:37 +0530

    Creating Complex Actions

--- src/state-management/TaskList.tsx
+++ src/state-management/TaskList.tsx
@@ -1,38 +1,33 @@
-import { useState } from 'react';
-
-interface Task {
-  id: number;
-  title: string;
-}
+import { useReducer, useState } from 'react';
+import tasksReducer from './reducers/tasksReducer';
 
 const TaskList = () => {
-  const [tasks, setTasks] = useState<Task[]>([]);
+  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, dispatch] = useReducer(tasksReducer, []);
 
   return (
     <>
       <button
         onClick={() =>
-          setTasks([
-            { id: Date.now(), title: 'Task ' + Date.now() },
-            ...tasks,
-          ])
+          dispatch({
+            type: 'ADD',
+            task: { id: Date.now(), title: 'Task ' + Date.now() },
+          })
         }
-        className="btn btn-primary my-3"
+        className='btn btn-primary my-3'
       >
         Add Task
       </button>
-      <ul className="list-group">
+      <ul className='list-group'>
         {tasks.map((task) => (
           <li
             key={task.id}
-            className="list-group-item d-flex justify-content-between align-items-center"
+            className='list-group-item d-flex justify-content-between align-items-center'
           >
-            <span className="flex-grow-1">{task.title}</span>
+            <span className='flex-grow-1'>{task.title}</span>
             <button
-              className="btn btn-outline-danger"
-              onClick={() =>
-                setTasks(tasks.filter((t) => t.id !== task.id))
-              }
+              className='btn btn-outline-danger'
+              onClick={() => dispatch({ type: 'DELETE', taskId: task.id })}
             >
               Delete
             </button>
