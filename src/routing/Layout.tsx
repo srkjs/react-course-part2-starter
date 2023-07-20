cd2632c6b481c8d9b9701e01faa9f06ff3c3af3c
swathir01 <53891830+srkjs@users.noreply.github.com>
2023-07-20 23:50:16 +0530

    Private Routes

--- src/routing/Layout.tsx
+++ src/routing/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 import NavBar from './NavBar';
 
 const Layout = () => {
-  throw new Error('Something went wrong!!!');
+  // throw new Error('Something went wrong!!!');
   return (
     <>
       <NavBar />
--- src/routing/route.tsx
+++ src/routing/route.tsx
@@ -6,6 +6,7 @@ import UserDetail from './UserDetail';
 import Layout from './Layout';
 import UsersPage from './UsersPage';
 import ErrorPage from './ErrorPage';
+import LoginPage from './LoginPage';
 {
   /* <BrowserRouter /> */
 }
@@ -18,6 +19,7 @@ const router = createBrowserRouter([
     children: [
       // { path: '', element: <HomePage /> },
       { index: true, element: <HomePage /> },
+      { path: '/login', element: <LoginPage /> },
       {
         path: 'users',
         element: <UsersPage />,
