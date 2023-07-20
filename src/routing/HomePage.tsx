20c876c951f86cc8721e786a5aa229779beff1fd
swathir01 <53891830+srkjs@users.noreply.github.com>
2023-07-20 22:50:49 +0530

    Navigation

--- src/routing/HomePage.tsx
+++ src/routing/HomePage.tsx
@@ -1,11 +1,14 @@
+import { Link } from 'react-router-dom';
+
 const HomePage = () => {
   return (
     <>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-        Incidunt, mollitia!
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt,
+        mollitia!
       </p>
-      <a href="/users">Users</a>
+      {/* Using <Link> tag, the content of the UserList is already shifted to client, so there is no requirement for server call which Users link is clicked */}
+      <Link to={'/users'}>Users</Link>
     </>
   );
 };
