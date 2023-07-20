8e92b871814682a79c02a9aa7f38519cabb6f31a
swathir01 <53891830+srkjs@users.noreply.github.com>
2023-07-20 06:49:28 +0530

    Exercise- Creating a Provider

---
+++ src/state-management/hooks/useTasks.tsx
@@ -0,0 +1,8 @@
+import React, { useContext } from 'react';
+import TasksContext from '../contexts/tasksContext';
+
+const useTasks = () => {
+  return useContext(TasksContext);
+};
+
+export default useTasks;
